8a2a0001c92f67348a76b5b8b1620a0b41d4d42a
akhfa <akhmadfakhoni@gmail.com>
2015-11-16 13:24:49 +0700

    nambah kelas response helper

---
+++ src/helper/Response.java
@@ -0,0 +1,11 @@
+package helper;
+
+/**
+ * Created by akhfa on 16/11/15.
+ */
+public class Response {
+    public static String returnDatabaseError()
+    {
+        return "Parse response from database error";
+    }
+}
