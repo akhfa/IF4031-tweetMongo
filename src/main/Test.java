9d9209ffb933f7e47a15d90e7487f5962069fe45
akhfa <akhmadfakhoni@gmail.com>
2015-11-19 08:18:31 +0700

    nambah testcase

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,6 +1,11 @@
 package main;
 
+import model.Follower;
+import model.Friend;
+import model.Tweet;
 import model.User;
+
+import java.util.Date;
 
 /**
  * Created by akhfa on 16/11/15.
@@ -13,10 +18,20 @@ public class Test {
 
     private static void userTest()
     {
-        User user = new User("akhfa9", "akhfa");
-        if(user.save())
-            System.out.println("Success");
-        else
-            System.out.println("failed");
+//        User user = new User("akhfa9", "akhfa");
+//        if(user.save())
+//            System.out.println("Success");
+//        else
+//            System.out.println("failed");
+//        System.out.print(User.isExist("akhfa9"));
+
+//        Friend friend = new Friend("a", "b", new Date());
+//        friend.save();
+
+//        Follower follower = new Follower("a", "b", new Date());
+//        follower.save();
+
+        Tweet tweet = new Tweet("akhfa", "siiipppp");
+        System.out.print(tweet.save());
     }
 }
