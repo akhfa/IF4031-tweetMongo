3aa5852abb6dcb2576f4d5da989b9eba92f98739
akhfa <akhmadfakhoni@gmail.com>
2015-11-19 07:46:26 +0700

    nambah fungsi gettable

--- src/model/Connection.java
+++ src/model/Connection.java
@@ -1,6 +1,7 @@
 package model;
 
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 import java.net.UnknownHostException;
@@ -25,5 +26,25 @@ public class Connection {
         }
 
         return db;
+    }
+
+    public static DBCollection getTable(String tableName)
+    {
+        DBCollection table = null;
+        try
+        {
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+            DB db = mongoClient.getDB("13513601");
+
+            table = db.getCollection(tableName);
+
+        }catch (UnknownHostException e)
+        {
+            System.out.println("cannot connect to database.");
+            System.exit(1);
+        }
+
+        return table;
     }
 }
