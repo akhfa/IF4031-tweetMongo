8d6bd110bd27af6759e19b7287f6bdd355e63e04
akhfa <akhmadfakhoni@gmail.com>
2015-11-19 08:09:36 +0700

    added follower save

--- src/model/Follower.java
+++ src/model/Follower.java
@@ -1,7 +1,52 @@
 package model;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import helper.Response;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.Date;
+
 /**
  * Created by akhfa on 16/11/15.
  */
 public class Follower {
+    private String username;
+    private String follower;
+    private Date timestamp;
+
+    public Follower(String _username, String _follower, Date _timestamp)
+    {
+        this.username = _username;
+        this.follower = _follower;
+        this.timestamp = _timestamp;
+    }
+
+    public boolean save()
+    {
+        DBCollection table = Connection.getTable("followers");
+
+        BasicDBObject document = new BasicDBObject();
+        document.put("username", username);
+        document.put("follower", follower);
+        document.put("timestamp", timestamp);
+
+        JSONParser parser = new JSONParser();
+
+        JSONObject response = null;
+
+        try
+        {
+            response = (JSONObject)parser.parse(table.insert(document).toString());
+        }
+        catch (ParseException e)
+        {
+            System.out.println(Response.returnDatabaseError());
+            System.exit(1);
+        }
+
+        return response.get("ok").equals(1l);
+    }
 }
