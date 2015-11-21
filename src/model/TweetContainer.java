2229589ffbe56a1964aae76b7b786e0e489c5e4c
akhfa <akhmadfakhoni@gmail.com>
2015-11-21 18:24:49 +0700

    Added tweet container

--- src/model/TweetContainer.java
+++ src/model/TweetContainer.java
@@ -4,4 +4,21 @@ package model;
  * Created by akhfa on 16/11/15.
  */
 public class TweetContainer {
+    private String username;
+    private String body;
+    public TweetContainer(String _username, String _body)
+    {
+        this.username = _username;
+        this.body = _body;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getBody()
+    {
+        return body;
+    }
 }
