ffdbd51badbf2f440d4e92f908c53cdd579bc6ef
akhfa <akhmadfakhoni@gmail.com>
2015-11-23 06:19:02 +0700

    done, need through testing

---
+++ src/controller/Logic.java
@@ -0,0 +1,98 @@
+package controller;
+
+import model.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by akhfa on 21/11/15.
+ */
+public class Logic {
+    /**
+     * Mengembalikan String username jika username ada di database.
+     * Jika tidak ada, mengembalikan string false
+     * @param username
+     * @param password
+     * @return
+     */
+    public boolean register(String username, String password)
+    {
+        User user = new User(username, password);
+
+        if(!user.exist())
+        {
+            user.save();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean login(String _username, String _password)
+    {
+        User user = new User(_username, _password);
+        return user.login();
+    }
+
+    public boolean follow(String from, String destination)
+    {
+        User user1 = new User(from);
+        User user2 = new User(destination);
+        if(user2.exist())
+        {
+            Follower followerManajer = new Follower(destination, from, new Date());
+            followerManajer.save();
+            Friend friendManajer = new Friend(from, destination, new Date());
+            friendManajer.save();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public boolean postTweet(String _username, String _body)
+    {
+        Date timeuuid = new Date();
+        Tweet tweet = new Tweet(_username, _body);
+        String tweetuuid = tweet.save();
+        UserLine userline = new UserLine(_username, timeuuid, tweetuuid);
+        userline.save();
+        TimeLine timeline = new TimeLine(_username, timeuuid, tweetuuid);
+        timeline.save();
+
+        ArrayList<String> daftarFollower = Follower.getAllFollowerFrom(_username);
+        for(String follower : daftarFollower)
+        {
+            System.out.println("follower = " + follower);
+            TimeLine timelineTemp = new TimeLine(follower, timeuuid, tweetuuid);
+            timelineTemp.save();
+        }
+
+        return true;
+    }
+
+    public void printUserline(String _username)
+    {
+        ArrayList<TweetContainer> tweets = UserLine.getAllTweetFrom(_username);
+        for(TweetContainer tweet : tweets)
+        {
+            System.out.println("@ " + tweet.getUsername());
+            System.out.println("" + tweet.getBody());
+            System.out.println("===================================");
+        }
+    }
+
+    public void printTimeline(String _username)
+    {
+        ArrayList<TweetContainer> tweets = TimeLine.getAllTweetFrom(_username);
+        for(TweetContainer tweet : tweets)
+        {
+            System.out.println("@ " + tweet.getUsername());
+            System.out.println(tweet.getBody());
+            System.out.println("===================================");
+        }
+    }
+}
--- src/main/TweetMongo.java
+++ src/main/TweetMongo.java
@@ -4,36 +4,125 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import controller.Logic;
 
 import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Created by akhfa on 16/11/15.
  */
 
 public class TweetMongo {
-    public static void main(String [] args)
-    {
-        try
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String username = "";
+        boolean guest = true;
+        boolean first = true;
+        Logic logic = new Logic();
+
+        while (true)
         {
-            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            if(guest)
+            {
+                if(first)
+                {
+                    System.out.println("Selamat Datang");
+                    first = false;
+                }
 
-            DB db = mongoClient.getDB("13513601");
+                System.out.println("To register,    ketik: reg [username] [password]");
+                System.out.println("To login,       ketik: login [username] [password]");
+                System.out.println("To exit,        ketik: exit");
+                Scanner in = new Scanner(System.in);
+                System.out.print("guest:~$ ");
+                String command = in.nextLine();
 
-            DBCollection table = db.getCollection("users");
+                String [] com = command.split(" ");
+                switch(com[0])
+                {
+                    case "reg":
+                        if(logic.register(com[1], com[2]))
+                        {
+                            username = com[1];
+                            first = true;
+                            guest = false;
+                        }
+                        else
+                        {
+                            System.out.println("User exist");
+                        }
+                        break;
+                    case "login":
+                        if(logic.login(com[1], com[2]))
+                        {
+                            username = com[1];
+                            first = true;
+                            guest = false;
+                        }
+                        else
+                            System.out.println("Password salah. Coba lagi.");
+                        break;
+                    case "exit":
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Wrong command");
+                }
+            }
+            else
+            {
+                if(first)
+                {
+                    System.out.println("Selamat datang " + username);
+                    first = false;
+                }
+                System.out.println("To follow,          ketik: follow [username]");
+                System.out.println("To send tweet,      ketik: tweet [body]");
+                System.out.println("To see userline,    ketik: userline");
+                System.out.println("To see timeline,    ketik: timeline");
+                System.out.println("To logout,          ketik: logout");
+                Scanner in = new Scanner(System.in);
+                System.out.print(username + ":~$ ");
+                String command = in.nextLine();
 
-            BasicDBObject document = new BasicDBObject();
-
-            document.put("name", "akhfa3");
-            document.put("umur", "22");
-            document.put("createdDate", new Date());
-
-            System.out.println(table.insert(document));
-        }catch (UnknownHostException e)
-        {
-
+                String [] com = command.split(" ",2);
+                switch(com[0])
+                {
+                    case "follow":
+                        if(logic.follow(username, com[1]))
+                        {
+                            System.out.println("You follow " + com[1]);
+                        }
+                        else
+                        {
+                            System.out.println(com[1] + " not exist. Coba lagi.");
+                        }
+                        break;
+                    case "tweet":
+                        if(logic.postTweet(username, com[1]))
+                        {
+                            System.out.println("Tweet successfully posted");
+                        }
+                        else
+                        {
+                            System.out.println("Tweet not successfully posted");
+                        }
+                        break;
+                    case "userline":
+                        logic.printUserline(username);
+                        break;
+                    case "timeline":
+                        logic.printTimeline(username);
+                        break;
+                    case "logout":
+                        guest = true;
+                        break;
+                    default:
+                        System.out.println("Wrong command");
+                }
+            }
         }
-
     }
 }
